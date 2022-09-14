6deae6ff575bc46bac5899f79bc076f305096bfa
LengNan777 <L2452520160@gmail.com>
2022-09-14 15:55:19 -0500

    Finish lab2

--- Program.cs
+++ Program.cs
@@ -1,31 +1,33 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
 
-lab1Delegate del = new lab1Delegate(concatenatedString);
-string testString = "Test";
-Console.WriteLine(del(testString, 5));
+LinkedList<String> linkList1 = new LinkedList<String>();
 
-lab1Delegate del2 = new lab1Delegate(removeLetters);
-Console.WriteLine(del2(testString, 3));
-Console.WriteLine(testString);
-Console.WriteLine(del2(testString, 5));
+//The LinkedList needs to be able to Add new Nodes to the List
+linkList1.AddLast("First");
+linkList1.AddLast("Second");
+linkList1.AddLast("Third");
 
-string concatenatedString(string wordArg, int numArg)
+//The Node needs the ability to print its own data
+Console.WriteLine(linkList1.First.Value);
+Console.WriteLine(linkList1.Last.Value);
+
+//The LinkedList needs to be able to print the data of all of its Nodes in sequence
+foreach(String s in linkList1)
 {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < numArg; i++)
-    {
-        sb.Append(wordArg);
-    }
-    return sb.ToString();
+	Console.WriteLine(s);
 }
 
-string removeLetters(string wordArg, int numArg)
-{
-    if (numArg < wordArg.Length)
-    {
-        return wordArg.Substring(0, wordArg.Length - numArg);
-    }
-    return "";
-}
 
-delegate string lab1Delegate(string wordArg, int numArg);
+LinkedList<int> linkList2 = new LinkedList<int>();
+linkList2.AddLast(1);
+linkList2.AddLast(2);
+linkList2.AddLast(3);
+
+Console.WriteLine(linkList2.First.Value);
+Console.WriteLine(linkList2.Last.Value);
+
+foreach (int i in linkList2)
+{
+	Console.WriteLine(i);
+}
